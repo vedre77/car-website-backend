69d3b4fe073804ed31edf9db2a137eb8d58282b2
vedre77 <vedranvitas@net.hr>
2023-03-27 21:04:22 +0200

    Converted pricing-service into a heureka client/ microservice

---
+++ pricing-service/src/main/java/com/udacity/pricing/entity/Price.java
@@ -0,0 +1,62 @@
+package com.udacity.pricing.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.math.BigDecimal;
+
+/**
+ * Represents the price of a given vehicle, including currency.
+ */
+@Entity
+public class Price {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private String currency;
+    private BigDecimal price;
+    private Integer vehicleId;
+
+    public Price() {
+    }
+
+    public Price(Integer id, String currency, BigDecimal price, Integer vehicleId) {
+        this.currency = currency;
+        this.price = price;
+        this.vehicleId = vehicleId;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public Integer getVehicleId() {
+        return vehicleId;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setVehicleId(Integer vehicleId) {
+        this.vehicleId = vehicleId;
+    }
+}
---
+++ pricing-service/src/main/java/com/udacity/pricing/repository/PriceRepository.java
@@ -0,0 +1,8 @@
+package com.udacity.pricing.repository;
+
+import com.udacity.pricing.entity.Price;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PriceRepository extends JpaRepository<Price, Integer> {
+
+}
