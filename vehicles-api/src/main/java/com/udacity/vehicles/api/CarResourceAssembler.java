732bfc54ce8cd4eaf5167939222e9aff00b2b55d
vedre77 <vedranvitas@net.hr>
2023-03-29 12:54:28 +0200

    Added Create car (post) method

--- vehicles-api/src/main/java/com/udacity/vehicles/api/CarResourceAssembler.java
+++ vehicles-api/src/main/java/com/udacity/vehicles/api/CarResourceAssembler.java
@@ -4,11 +4,18 @@ import com.udacity.vehicles.domain.car.Car;
 import org.springframework.hateoas.Resource;
 import org.springframework.hateoas.ResourceAssembler;
 import org.springframework.stereotype.Component;
-
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.*;
 
 /**
  * Maps the CarController to the Car class using HATEOAS
+ * HATEOAS stands for "Hypermedia as the Engine of Application State"
+ * and is a constraint of the REST architecture style. It means that a client interacts with
+ * a web service entirely through hypermedia provided dynamically by the server.
+ * In the context of Spring, HATEOAS is implemented through the Spring HATEOAS module,
+ * which provides a set of tools for building hypermedia-driven RESTful web services.
+ * The module includes several classes and interfaces that enable developers to easily create
+ * and manipulate links between resources, define resource representations using the HAL format
+ * (Hypertext Application Language), and create custom media types for their APIs.
  */
 @Component
 public class CarResourceAssembler implements ResourceAssembler<Car, Resource<Car>> {
