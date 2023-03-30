1e1e4440a3398df0821a6d30c0b7e179df0af655
vedre77 <vedranvitas@net.hr>
2023-03-30 16:02:43 +0200

    Added @DeleteMapping

--- vehicles-api/src/main/java/com/udacity/vehicles/api/CarController.java
+++ vehicles-api/src/main/java/com/udacity/vehicles/api/CarController.java
@@ -100,6 +100,7 @@ class CarController {
         /**
          * TODO: Use the Car Service to delete the requested vehicle.
          */
+        carService.delete(id);
         return ResponseEntity.noContent().build();
     }
 }
