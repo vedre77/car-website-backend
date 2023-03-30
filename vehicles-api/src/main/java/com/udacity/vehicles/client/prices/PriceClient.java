d62a64df550e1e5d754c1010b1953ebcaa7da9b9
vedre77 <vedranvitas@net.hr>
2023-03-30 11:55:52 +0200

    Modified the get request to function to the builder instantiation

--- vehicles-api/src/main/java/com/udacity/vehicles/client/prices/PriceClient.java
+++ vehicles-api/src/main/java/com/udacity/vehicles/client/prices/PriceClient.java
@@ -32,15 +32,15 @@ public class PriceClient {
      */
     public String getPrice(Long vehicleId) {
         try {
-            Price price = client
-                    .get()
-                    .uri(uriBuilder -> uriBuilder
-                            .path("services/price/")
-                            .queryParam("vehicleId", vehicleId)
-                            .build()
-                    )
-                    .retrieve().bodyToMono(Price.class).block();
-
+            Price price = client.get()
+                                .uri(uriBuilder -> uriBuilder
+                                    .path("/prices/{id}")
+                                    .build(vehicleId)
+                                )
+                                .exchange()
+                                .block()
+                                .bodyToMono(Price.class)
+                                .block();
             return String.format("%s %s", price.getCurrency(), price.getPrice());
 
         } catch (Exception e) {
