6794dea91f11c984e128de36916acca845e95c11
vedre77 <vedranvitas@net.hr>
2023-03-30 21:34:20 +0200

    Converted Address to JPA entity, added carId field

--- boogle-maps/src/main/java/com/udacity/boogle/maps/Address.java
+++ boogle-maps/src/main/java/com/udacity/boogle/maps/Address.java
@@ -1,10 +1,21 @@
 package com.udacity.boogle.maps;
 
+import javax.annotation.processing.Generated;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 /**
  * Declares a class to store an address, city, state and zip code.
  */
+@Entity
 public class Address {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long addressId;
+    private Long carId;
     private String address;
     private String city;
     private String state;
@@ -18,6 +29,14 @@ public class Address {
         this.city = city;
         this.state = state;
         this.zip = zip;
+    }
+
+    public Long getAddressId() {
+        return addressId;
+    }
+
+    public Long getCarId() {
+        return carId;
     }
 
     public String getAddress() {
@@ -50,6 +69,14 @@ public class Address {
 
     public void setZip(String zip) {
         this.zip = zip;
+    }
+
+    public void setAddressId(Long addressId) {
+        this.addressId = addressId;
+    }
+
+    public void setCarId(Long carId) {
+        this.carId = carId;
     }
 
     @Override
