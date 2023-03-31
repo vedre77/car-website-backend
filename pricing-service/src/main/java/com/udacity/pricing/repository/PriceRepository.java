443fc3d0647bb8166f4ea125f91f81ba628f07ff
vedre77 <vedranvitas@net.hr>
2023-03-31 17:03:39 +0200

    Added lookup and by-field retrieval methods

--- pricing-service/src/main/java/com/udacity/pricing/repository/PriceRepository.java
+++ pricing-service/src/main/java/com/udacity/pricing/repository/PriceRepository.java
@@ -4,5 +4,6 @@ import com.udacity.pricing.entity.Price;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PriceRepository extends JpaRepository<Price, Integer> {
-
+    boolean existsByVehicleId(Long vehicleId);
+    Price findByVehicleId(Long vehicleId);
 }
