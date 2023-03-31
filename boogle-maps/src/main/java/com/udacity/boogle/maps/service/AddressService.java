ad1f630764c1330ae82668a2834b87b42d6cb5ed
vedre77 <vedranvitas@net.hr>
2023-03-31 08:07:19 +0200

    Added lat and long setting/ check

--- boogle-maps/src/main/java/com/udacity/boogle/maps/service/AddressService.java
+++ boogle-maps/src/main/java/com/udacity/boogle/maps/service/AddressService.java
@@ -3,6 +3,7 @@ package com.udacity.boogle.maps.service;
 import com.udacity.boogle.maps.Address;
 import com.udacity.boogle.maps.repository.AddressRepository;
 import org.springframework.stereotype.Service;
+import java.util.Objects;
 
 @Service
 public class AddressService {
@@ -13,26 +14,28 @@ public class AddressService {
         this.addressRepository = addressRepository;
     }
 
-    public Address searchCarAddress(Address randomAddress, Long id) {
+    public Address searchCarAddress(Address randomAddress, Long id, Double lat, Double lon) {
 
-
-        Boolean carAddressExists = addressRepository.existsByCarId(id);
+        boolean carAddressExists = addressRepository.existsByCarId(id);
         //System.out.println(carAddressExists);
         if (carAddressExists) {
+            // check for latitude and longitude change
             Address savedCarAddress = addressRepository.findByCarId(id);
-            return savedCarAddress;
-        } else {
-            Address newCarAddress = new Address();
-            newCarAddress.setCarId(id);
-            //
-            newCarAddress.setAddress(randomAddress.getAddress());
-            newCarAddress.setCity(randomAddress.getCity());
-            newCarAddress.setState(randomAddress.getState());
-            newCarAddress.setZip(randomAddress.getZip());
-
-            addressRepository.save(newCarAddress);
-
-            return newCarAddress;
+            if (Objects.equals(savedCarAddress.getLat(), lat) && Objects.equals(savedCarAddress.getLon(), lon)) {
+                return savedCarAddress;
+            } else {
+                addressRepository.delete(savedCarAddress);
+            }
         }
-    }
+        Address newCarAddress = new Address();
+        newCarAddress.setCarId(id);
+        newCarAddress.setLat(lat);
+        newCarAddress.setLon(lon);
+        newCarAddress.setAddress(randomAddress.getAddress());
+        newCarAddress.setCity(randomAddress.getCity());
+        newCarAddress.setState(randomAddress.getState());
+        newCarAddress.setZip(randomAddress.getZip());
+        addressRepository.save(newCarAddress);
+        return newCarAddress;
+        }
 }
