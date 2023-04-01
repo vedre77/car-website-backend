95aa0b4a8b32902e687155abdf937c81f05e4dd5
vedre77 <vedranvitas@net.hr>
2023-04-01 12:28:02 +0200

    App initialized

---
+++ customer/src/test/java/udacity/car/backend/customer/CustomerApplicationTests.java
@@ -0,0 +1,13 @@
+package udacity.car.backend.customer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
