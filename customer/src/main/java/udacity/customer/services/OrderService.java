9647e5d7b20a23ce00440c7b33cebd112472ab38
vedre77 <vedranvitas@net.hr>
2023-04-03 20:57:43 +0200

    Added order client and orderCar method

--- customer/src/main/java/udacity/customer/services/OrderService.java
+++ customer/src/main/java/udacity/customer/services/OrderService.java
@@ -1,6 +1,8 @@
 package udacity.customer.services;
 
 import org.springframework.stereotype.Service;
+import udacity.customer.client.OrderClient;
+import udacity.customer.model.Item;
 import udacity.customer.model.Order;
 import udacity.customer.repository.OrderRepository;
 import java.util.List;
@@ -8,12 +10,20 @@ import java.util.List;
 @Service
 public class OrderService {
     private final OrderRepository orderRepository;
+    private final OrderClient orderClient;
 
-    public OrderService(OrderRepository orderRepository) {
+    public OrderService(OrderRepository orderRepository, OrderClient orderClient) {
         this.orderRepository = orderRepository;
+        this.orderClient = orderClient;
     }
 
     public List<Order> getAllOrders() {
         return orderRepository.findAll();
+    }
+
+    public void orderCar() {
+        // mock order
+        Item orderedItem = orderClient.getCar(1L);
+        System.out.println(orderedItem);
     }
 }
