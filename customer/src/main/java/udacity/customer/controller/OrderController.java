daaa3d90ec4fa01a62dde4d43311839f58dd915c
vedre77 <vedranvitas@net.hr>
2023-04-03 20:47:21 +0200

    Update to orderService call

--- customer/src/main/java/udacity/customer/controller/OrderController.java
+++ customer/src/main/java/udacity/customer/controller/OrderController.java
@@ -1,14 +1,10 @@
 package udacity.customer.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import udacity.customer.model.Order;
-import udacity.customer.model.User;
-import udacity.customer.repository.OrderRepository;
-import udacity.customer.repository.UserRepository;
 import udacity.customer.services.OrderService;
 
 import java.util.List;
@@ -21,6 +17,10 @@ public class OrderController {
 
     @GetMapping("/orders")
     public String getAllOrders(Model model) {
+
+        // crate and order for car 1!
+        orderService.orderCar();
+
         List<Order> allOrders = orderService.getAllOrders();
         model.addAttribute("orders", allOrders);
         return "order-list";
