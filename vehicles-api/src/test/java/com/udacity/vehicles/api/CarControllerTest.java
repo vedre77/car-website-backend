507d388717482dd5db98e7a31808eadcf48202cb
vedre77 <vedranvitas@net.hr>
2023-04-05 11:58:24 +0200

    Updated CarController tests, added PUT test

--- vehicles-api/src/test/java/com/udacity/vehicles/api/CarControllerTest.java
+++ vehicles-api/src/test/java/com/udacity/vehicles/api/CarControllerTest.java
@@ -2,9 +2,7 @@ package com.udacity.vehicles.api;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import com.udacity.vehicles.client.maps.MapsClient;
@@ -112,6 +110,21 @@ public class CarControllerTest {
     public void deleteCar() throws Exception {
         mvc.perform(delete("/cars/1"))
                 .andExpect(status().isNoContent());
+    }
+
+    @Test
+    public void updateCar() throws Exception {
+        Car car = getCar();
+        car.setId(5L);
+        car.setPrice("55555");
+        mvc.perform(
+            put(new URI("/cars/5"))
+                    .content(json.write(car).getJson())
+                    .contentType(MediaType.APPLICATION_JSON_UTF8)
+                    .accept(MediaType.APPLICATION_JSON_UTF8))
+                    .andExpect(jsonPath("$.details.model").value("Impala"))
+                    .andExpect(status().isOk()
+        );
     }
 
     /**
