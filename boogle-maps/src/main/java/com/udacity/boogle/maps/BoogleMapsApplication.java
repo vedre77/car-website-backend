0f099fef6a5659cd1f334344f4d6579f27cca5d9
vedre77 <vedranvitas@net.hr>
2023-04-05 08:10:41 +0200

    Added comment for terminal curl test command

--- boogle-maps/src/main/java/com/udacity/boogle/maps/BoogleMapsApplication.java
+++ boogle-maps/src/main/java/com/udacity/boogle/maps/BoogleMapsApplication.java
@@ -6,6 +6,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BoogleMapsApplication {
 
+	/**
+	 * Due to added functionality, the test curl command via the terminal
+	 * should include car ID, example:
+	 * curl -X GET "http://localhost:9191/maps/?lat=0.0&lon=0.0&carId=1"
+	 */
+
 	public static void main(String[] args) {
 		SpringApplication.run(BoogleMapsApplication.class, args);
 	}
